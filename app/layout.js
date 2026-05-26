044cbb3a3557ed69dc7698d2f3e47896c3ef5db9
casmir844-wq <casmir844@gmail.com>
2026-05-26 18:59:11 +0100

    Remove metadata export from layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,3 @@
-export const metadata = {
-  title: 'We-Mail 😎',
-  description: 'Futuristic productivity inbox',
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
