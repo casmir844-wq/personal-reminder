e07d65bc0853544df7ca7d38871467b3749954be
casmir844-wq <casmir844@gmail.com>
2026-05-26 18:09:02 +0100

    Add metadata and RootLayout component to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: 'We-Mail 😎',
+  description: 'Futuristic productivity inbox',
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
